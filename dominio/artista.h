4d3b3c823b6f78df0e31cf0e1e425f9bf82a895e
Flecchia Hernan <flecchia.hernan@gmail.com>
2026-06-11 09:19:35 -0300

    Agregar structs para persistir entidades

--- dominio/artista.h
+++ dominio/artista.h
@@ -1,11 +1,20 @@
 #ifndef ARTISTA_H
 #define ARTISTA_H
+#define DIM_NOMBRE 50
+#define DIM_GENERO 30
 
 typedef struct stArtista {
     int id;
-    char nombre[50];
-    char genero[30];
+    char nombre[DIM_NOMBRE];
+    char genero[DIM_GENERO];
 } Artista;
+
+typedef struct stArtistaArchivo {
+    int id;
+    char nombre[DIM_NOMBRE];
+    char genero[DIM_GENERO];
+    char valido; // 'S' para activo, 'N' para borrado lógico (Auditoría)
+} ArtistaArchivo;
 
 // Prototipo de la función para mostrar los datos del artista
 void mostrarArtista(Artista artista);

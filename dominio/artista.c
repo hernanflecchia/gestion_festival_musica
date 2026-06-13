ffb54b2d95e7dda3b4ef1a3a4c8672c97055be33
Hernan Flecchia <flecchia.hernan@gmail.com>
2026-06-13 11:52:09 -0300

    Guardando

--- dominio/artista.c
+++ dominio/artista.c
@@ -3,9 +3,9 @@
 #include "artista.h"
 
 void mostrarArtistaAdmin(Artista artista) {
-    printf("ID: %d | Nombre: %-20s | Genero: %s\n", artista.id, artista.nombre, artista.genero);
+    printf("ID: %d | Nombre: %-*s | Genero: %s\n", artista.id, DIM_ARTISTA_NOMBRE, artista.nombre, artista.genero);
 }
 
 void mostrarArtistaUsuario(Artista artista) {
-    printf("Nombre: %-20s | Genero: %s\n", artista.nombre, artista.genero);
+    printf("Nombre: %-*s | Genero: %s\n", DIM_ARTISTA_NOMBRE, artista.nombre, artista.genero);
 }
--- interfaz/menus.c
+++ interfaz/menus.c
@@ -2,6 +2,7 @@
 #include "scanner.h"
 #include "../dominio/usuario.h"
 #include "../dominio/artista.h"
+#include "../dominio/escenario.h"
 #include "../logica/colecciones.h"
 #include "../logica/gestor_archivos.h"
 
@@ -26,6 +27,11 @@ Usuarios pedirCredencialesUsuario(const char* titulo) {
     return usuario;
 }
 
+int pedirIdGenerico(const char* nombreEntidad) {
+    printf("\nIngrese el ID del %s: ", nombreEntidad);
+    return scanInt();
+}
+
 Artista pedirDatosNuevoArtista(void) {
     Artista nuevo;
     printf("\n--- ALTA DE ARTISTA ---\n");
@@ -34,11 +40,6 @@ Artista pedirDatosNuevoArtista(void) {
     printf("Ingrese el genero musical: ");
     scanString(nuevo.genero, DIM_GENERO);
     return nuevo;
-}
-
-int pedirIdGenerico(const char* nombreEntidad) {
-    printf("\nIngrese el ID del %s: ", nombreEntidad);
-    return scanInt();
 }
 
 Artista pedirDatosModificadosArtista(Artista artistaExistente) {
@@ -57,6 +58,33 @@ int mostrarMenuArtistas(void) {
     printf("2. Modificar Artista existente\n");
     printf("3. Dar de baja (Eliminar) Artista\n");
     printf("4. Listar Artistas por pantalla\n");
+    printf("0. Volver al menu principal\n");
+    printf("Elija una opcion: ");
+    return scanInt();
+}
+
+Escenario pedirDatosNuevoEscenario(void) {
+    Escenario nuevo;
+    printf("\n--- ALTA DE ESCENARIO ---\n");
+    printf("Ingrese el nombre del escenario: ");
+    scanString(nuevo.nombre, DIM_ESCENARIO_NOMBRE);
+    return nuevo;
+}
+
+Escenario pedirDatosModificadosEscenario(Escenario escenarioExistente) {
+    printf("\n--- MODIFICANDO ESCENARIO ---\n");
+    printf("Modificando los datos del ID: %d\n", escenarioExistente.id);
+    printf("Nuevo nombre (anterior: %s): ", escenarioExistente.nombre);
+    scanString(escenarioExistente.nombre, DIM_ESCENARIO_NOMBRE);
+    return escenarioExistente;
+}
+
+int mostrarMenuEscenarios(void) {
+    printf("\n--- GESTION DE ESCENARIOS ---\n");
+    printf("1. Dar de alta nuevo Escenario\n");
+    printf("2. Modificar Escenario existente\n");
+    printf("3. Dar de baja (Eliminar) Escenario\n");
+    printf("4. Listar Escenarios por pantalla\n");
     printf("0. Volver al menu principal\n");
     printf("Elija una opcion: ");
     return scanInt();
@@ -94,6 +122,14 @@ void menuUsuario(ColeccionArtistas* cArt, ColeccionEscenarios* cEsc, ColeccionPr
                         Artista artistas = obtenerArtista(cArt, i);
                         if (artistas.id != -1) {
                             mostrarArtistaUsuario(artistas);
+                        }
+                    }
+                    printf("¿Desea exportar los artistas a un TSV? (s/n)\n");
+                    if (confirmar('s')) {
+                        if (exportarArtistasATexto("artistas.tsv", cArt)) {
+                            printf("[Exito] El listado fue guardado en 'artistas.tsv'.\n");
+                        } else {
+                            printf("[Error] No se pudo crear el archivo de exportacion.\n");
                         }
                     }
                 }
@@ -241,8 +277,92 @@ void menuAdmin(Usuarios usuario, ColeccionArtistas* cArt, ColeccionEscenarios* c
                 break;
 
             case 2:
-                // Aca armarías el do-while para ESCENARIOS
-                printf("\n(Menu Escenarios en construccion...)\n");
+                do {
+                    opcionSubMenu = mostrarMenuEscenarios();
+                    switch(opcionSubMenu) {
+                        case 1:
+                        {
+                            Escenario nuevoEscenario = pedirDatosNuevoEscenario();
+                            nuevoEscenario.id = obtenerSiguienteIdEscenario(cEsc);
+                            // Intentamos agregarlo a la memoria dinámica
+                            if (agregarEscenario(cEsc, nuevoEscenario) == 1) {
+                                // Si la memoria RAM lo aceptó, lo mapeamos al formato de archivo
+                                EscenarioArchivo nuevoArch = transformarAEscenarioArchivo(nuevoEscenario);
+                                // Finalmente lo guardamos en el disco
+                                if (guardarEscenarioEnArchivo(nuevoArch) == 1) {
+                                    printf("\n[Exito] Escenario agregado y guardado correctamente.\n");
+                                } else {
+                                    printf("\n[Error] El escenario esta en memoria pero fallo el guardado en disco.\n");
+                                }
+                                
+                            } else {
+                                printf("\n[Error] No se pudo agregar al escenario por falta de memoria.\n");
+                            }
+                            break;
+                        }
+                        case 2:
+                            idBuscado = pedirIdGenerico("Escenario a modificar");
+                            indice = buscarIndiceEscenarioPorId(cEsc, idBuscado);
+                            if (indice != -1) {
+                                Escenario aModificar = obtenerEscenario(cEsc, indice);
+                                aModificar = pedirDatosModificadosEscenario(aModificar);                             
+                                if (actualizarEscenario(cEsc, indice, aModificar) == 1) {
+                                    EscenarioArchivo archModificado = transformarAEscenarioArchivo(aModificar);
+                                    if (modificarEscenarioEnArchivo(archModificado) == 1) {
+                                        printf("\n[Exito] Escenario modificado en memoria y disco.\n");
+                                    } else {
+                                        printf("\n[Error] Modificado en memoria pero no en disco.\n");
+                                    }
+                                }
+                            } else {
+                                printf("\n[Error] Indice invalido. No existe el escenario.\n");
+                            }
+                            break;
+                        case 3:
+                            idBuscado = pedirIdGenerico("Escenario a dar de baja");
+                            indice = buscarIndiceEscenarioPorId(cEsc, idBuscado);
+                            if (indice != -1) {
+                                eliminarEscenarioDeMemoria(cEsc, indice);
+                                if (bajaLogicaEscenarioEnArchivo(idBuscado) == 1) {
+                                    printf("\n[Exito] Escenario borrado de la memoria y del disco correctamente.\n");
+                                    // Borrar en cascada
+                                    int cantMemoria = eliminarPresentacionesDeMemoriaPorEscenario(cPres, idBuscado);
+                                    int cantDisco = bajaLogicaPresentacionesEnArchivoPorEscenario(idBuscado);
+                                    // Verificamos si hubo presentaciones afectadas
+                                    if (cantMemoria > 0 || cantDisco > 0) {
+                                        printf("[Exito] Se borraron %d presentaciones en Memoria y %d en disco asociadas a este escenario.\n", cantMemoria, cantDisco);
+                                    } else {
+                                        printf("[Info] El escenario no tenia presentaciones programadas activas.\n");
+                                    }
+                                } else {
+                                    printf("\n[Error] Se borró de memoria pero falló la escritura en el archivo.\n");
+                                }
+                            } else {
+                                printf("\n[Error] El ID ingresado no existe o ya fue dado de baja previamente.\n");
+                            }
+                            break;
+                        case 4:
+                            printf("\n--- LISTADO DE ESCENARIOS ---\n");
+                            int cantidad = obtenerCantidadEscenarios(cEsc);
+                            if (cantidad == 0) {
+                                printf("No hay escenarios cargados en el sistema.\n");
+                            } else {
+                                for (int i = 0; i < cantidad; i++) {
+                                    Escenario unEscenario = obtenerEscenario(cEsc, i);
+                                    // Verificamos que no sea el valor centinela de error (-1)
+                                    if (unEscenario.id != -1) {
+                                        mostrarEscenarioAdmin(unEscenario);
+                                    }
+                                }
+                            }
+                            break;
+                        case 0:
+                            // Simplemente vuelve al menú principal
+                            break;
+                        default:
+                            printf("\nOpcion invalida.\n");
+                    }
+                } while(opcionSubMenu != 0);
                 break;
                 
             case 3:

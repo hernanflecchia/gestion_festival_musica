4f472da43445e5f04e5e6998a7ad9a8cfbf3d417
adriel9-5 <adriel39102248@gmail.com>
2026-06-13 17:31:56 -0300

    nueva rama

---
+++ logica/coleccion_presentaciones.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "coleccion_presentaciones.h"
+
+ColeccionPresentaciones inicializarColeccionPresentaciones(void) {
+    ColeccionPresentaciones col;
+    
+    // Definimos un tamaño inicial arbitrario (por ejemplo, para 10 presentaciones)
+    col.capacidad = 10;
+    col.validos = 0;
+    // Solicitamos el bloque de memoria dinámico
+    col.arreglo = (Presentacion*) malloc(col.capacidad * sizeof(Presentacion));
+    // Verificamos que el sistema operativo nos haya dado la memoria
+    if (col.arreglo == NULL) {
+        printf("[Error Critico] No hay memoria disponible para inicializar Presentaciones.\n");
+        col.capacidad = 0;
+    }
+    
+    return col;
+}
+
+int agregarPresentacion(ColeccionPresentaciones* coleccion, Presentacion unaPresentacion) {
+    
+    // Verificamos si NO hay espacio (los válidos alcanzaron la capacidad)
+    if (coleccion->validos == coleccion->capacidad) {
+        
+        int nuevaCapacidad = coleccion->capacidad + 10;
+        Presentacion* nuevoBloque = (Presentacion*) realloc(coleccion->arreglo, nuevaCapacidad * sizeof(Presentacion));
+        
+        if (nuevoBloque == NULL) {
+            return 0; // Falló la inserción
+        }
+        
+        // Actualizamos los datos de la colección
+        coleccion->arreglo = nuevoBloque;
+        coleccion->capacidad = nuevaCapacidad;
+    }
+    
+    // Agregamos el presentacion a la coleccion
+    coleccion->arreglo[coleccion->validos] = unaPresentacion;
+    coleccion->validos++;
+    
+    return 1;
+}
+
+void liberarColeccionPresentaciones(ColeccionPresentaciones* coleccion) {
+    // Verificamos que el puntero no sea nulo antes de liberar
+    if (coleccion->arreglo != NULL) {
+        free(coleccion->arreglo);
+    }
+}
+
+int eliminarPresentacionesDeMemoriaPorArtista(ColeccionPresentaciones* coleccion, int idArtista) {
+    int cantidadBorradas = 0;
+    int i = 0;
+    
+    while (i < coleccion->validos) {
+        if (coleccion->arreglo[i].idArtista == idArtista) {
+            // Borrado físico: Pisamos moviendo todo hacia la izquierda
+            for (int j = i; j < coleccion->validos - 1; j++) {
+                coleccion->arreglo[j] = coleccion->arreglo[j + 1];
+            }
+            coleccion->validos--;
+            cantidadBorradas++;
+        } else {
+            i++; // Solo avanzamos si no borramos nada
+        }
+    }
+    
+    return cantidadBorradas;
+}
+
+int eliminarPresentacionesDeMemoriaPorEscenario(ColeccionPresentaciones* coleccion, int idEscenario) {
+    int cantidadBorradas = 0;
+    int i = 0;
+    
+    while (i < coleccion->validos) {
+        if (coleccion->arreglo[i].idEscenario == idEscenario) {
+            // Borrado físico: Pisamos moviendo todo hacia la izquierda
+            for (int j = i; j < coleccion->validos - 1; j++) {
+                coleccion->arreglo[j] = coleccion->arreglo[j + 1];
+            }
+            coleccion->validos--;
+            cantidadBorradas++;
+        } else {
+            i++; // Solo avanzamos si no borramos nada
+        }
+    }
+    
+    return cantidadBorradas;
+}
---
+++ logica/gestor_archivo_presentaciones.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string.h>
+#include "gestor_archivo_presentaciones.h"
+#include "../dominio/tipos_seguros.h"
+
+Presentacion transformarAPresentacionMemoria(PresentacionArchivo presentacionDisco) {
+    Presentacion presentacionMemoria;
+
+    presentacionMemoria.id = presentacionDisco.id;
+    presentacionMemoria.idArtista = presentacionDisco.idArtista;
+    presentacionMemoria.idEscenario = presentacionDisco.idEscenario;
+    presentacionMemoria.inicio = crearHorario(presentacionDisco.inicio.horas, presentacionDisco.inicio.minutos);
+    presentacionMemoria.duracion = crearDuracion(presentacionDisco.duracion.horas, presentacionDisco.duracion.minutos);
+
+    return presentacionMemoria;
+}
+
+void cargarPresentacionesDesdeArchivo(ColeccionPresentaciones* coleccion) {
+    FILE* arch = fopen(ARCHIVO_PRESENTACIONES, "rb");
+
+    if (arch != NULL) {
+        PresentacionArchivo presentacionLeido;
+        int agregado;
+
+        while (fread(&presentacionLeido, sizeof(PresentacionArchivo), 1, arch)) {
+            if (presentacionLeido.valido == 'S') {
+                Presentacion presentacionMemoria = transformarAPresentacionMemoria(presentacionLeido);
+                agregado = agregarPresentacion(coleccion, presentacionMemoria);
+                if (agregado == 0) {
+                    printf("[Error] No hay memoria disponible para ampliar el arreglo.\n");
+                    fclose(arch);
+                    return;
+                }
+            }
+        }
+        fclose(arch);
+    }
+}
+
+int bajaLogicaPresentacionesEnArchivoPorArtista(int idArtistaBuscado) {
+    int cantidadBorradas = 0;
+    FILE* arch = fopen(ARCHIVO_PRESENTACIONES, "r+b"); 
+    
+    if (arch != NULL) {
+        PresentacionArchivo leida;
+        while (fread(&leida, sizeof(PresentacionArchivo), 1, arch) > 0) { 
+            // Buscamos todas las presentaciones activas de ese artista
+            if (leida.idArtista == idArtistaBuscado && leida.valido == 'S') {
+                leida.valido = 'N'; // Las marcamos como borradas lógicamente
+                fseek(arch, -1 * sizeof(PresentacionArchivo), SEEK_CUR); 
+                fwrite(&leida, sizeof(PresentacionArchivo), 1, arch);
+                // Refrescamos el buffer haciendo un seek vacio
+                fseek(arch, 0, SEEK_CUR);
+                cantidadBorradas++;
+            }
+        }
+        fclose(arch);
+    }
+    return cantidadBorradas;
+}
+
+int bajaLogicaPresentacionesEnArchivoPorEscenario(int idEscenarioBuscado) {
+    int cantidadBorradas = 0;
+    FILE* arch = fopen(ARCHIVO_PRESENTACIONES, "r+b"); 
+    
+    if (arch != NULL) {
+        PresentacionArchivo leida;
+        while (fread(&leida, sizeof(PresentacionArchivo), 1, arch) > 0) { 
+            // Buscamos todas las presentaciones activas de ese escenario
+            if (leida.idEscenario == idEscenarioBuscado && leida.valido == 'S') {
+                leida.valido = 'N'; // Las marcamos como borradas lógicamente
+                fseek(arch, -1 * sizeof(PresentacionArchivo), SEEK_CUR); 
+                fwrite(&leida, sizeof(PresentacionArchivo), 1, arch);
+                // Refrescamos el buffer haciendo un seek vacio
+                fseek(arch, 0, SEEK_CUR);
+                cantidadBorradas++;
+            }
+        }
+        fclose(arch);
+    }
+    return cantidadBorradas;
+}

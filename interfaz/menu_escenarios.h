174f2f08401504dcd767b607fe7e1e382617a932
Hernan Flecchia <flecchia.hernan@gmail.com>
2026-06-13 20:02:20 -0300

    Agregar isAdmin en mostrarListadoPresentaciones

--- interfaz/menu_escenarios.h
+++ interfaz/menu_escenarios.h
@@ -1,11 +1,12 @@
 #ifndef MENU_ESCENARIOS_H
 #define MENU_ESCENARIOS_H
 
+#include <stdbool.h>
 #include "../dominio/escenario.h"
 #include "../logica/coleccion_escenarios.h"
 
 int mostrarMenuEscenarios(void);
-void mostrarListadoEscenarios(ColeccionEscenarios* colEscenarios);
+void mostrarListadoEscenarios(ColeccionEscenarios* colEscenarios, bool esAdmin);
 Escenario pedirDatosNuevoEscenario(void);
 Escenario pedirDatosModificadosEscenario(Escenario escenarioExistente);
 

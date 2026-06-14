e0f587e8b30218176c531713f10149e9ddcc6a04
MatiasBustos <matiasbustos2411@gmail.com>
2026-06-14 11:44:19 -0300

    Add main menu display function in menu_inicio.c

---
+++ interfaz/menu_inicio.c
@@ -0,0 +1,14 @@
+#include "menu_usuario.h"
+
+int mostrarMenuinicio(void)
+{
+    printf("======================\n");
+    printf("        INICIO \n");
+    printf("======================\n\n");
+    printf("    1. Iniciar sesion \n");
+    printf("    2. Crear usuario \n");
+    printf("    3. Salir \n");
+    
+    printf("Seleccione una opción: ");
+return scanInt();
+}

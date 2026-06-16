c03b7e389fdee9f525778fd0536be356b62e7878
MatiasBustos <matiasbustos2411@gmail.com>
2026-06-16 02:15:25 -0300

    Update menu_usuario.c

---
+++ interfaz/menu_usuario.c
@@ -0,0 +1,38 @@
+void menuUsuario(Usuarios usuario, ColeccionArtistas* colArtistas, ColeccionEscenarios* colEscenarios, ColeccionPresentaciones* colPresentaciones) {
+    int opcionMenu;
+    do {
+        opcionMenu = mostrarMenuDeUsuario(usuario);
+
+        switch(opcionMenu) {
+        case 1:
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        default:
+            printf("\nOpcion invalida. Intente de nuevo.\n");
+            break;
+        }
+    }
+    while(opcionMenu != 4);
+}
+
+int mostrarMenuUsuario(Usuarios usuario) {
+    printf(" Usuario: %s\n", usuario.nombre);
+    printf("|====================================|\n");
+    printf("|            MENU USUARIO            |\n");
+    printf("|====================================|\n");
+    printf("|                                    |\n");
+    printf("|   1. Ver artistas                  |\n");
+    printf("|   2. Ver escenarios                |\n");
+    printf("|   3. Ver presentaciones            |\n");
+    printf("|   4. Salir                         |\n");
+    printf("|                                    |\n");
+    printf("|------------------------------------|\n");
+    printf("|Seleccione una opcion: ");
+    return scanInt();
+}

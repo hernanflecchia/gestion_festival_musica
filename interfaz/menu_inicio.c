144616ef77f8329cc0e527efefd713c97e4b7f15
MatiasBustos <matiasbustos2411@gmail.com>
2026-06-16 01:26:24 -0300

    Refactor menu display for better formatting

--- interfaz/menu_inicio.c
+++ interfaz/menu_inicio.c
@@ -4,13 +4,15 @@
 
 int mostrarMenuInicio(void)
 {
-    printf("======================\n");
-    printf("        INICIO \n");
-    printf("======================\n\n");
-    printf("    1. Iniciar sesion \n");
-    printf("    2. Crear usuario \n");
-    printf("    3. Salir \n");
-    
-    printf("Seleccione una opción: ");
+    printf("|====================================|\n");
+    printf("|               INICIO               |\n");
+    printf("|====================================|\n");
+    printf("|                                    |\n");
+    printf("|   1. Iniciar sesion                |\n");
+    printf("|   2. Crear usuario                 |\n");
+    printf("|   3. Salir                         |\n");
+    printf("|                                    |\n");
+    printf("|------------------------------------|\n");
+    printf("|Seleccione una opcion: ");
 return scanInt();
 }
